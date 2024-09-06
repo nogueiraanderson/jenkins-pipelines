070362083039d6e5f3b421c0a96acf8605e3e25f
Peter Sirotnak <sirotnak.p@gmail.com>
2024-09-06 14:10:20 +0200

    PMM-7 User assigment as root

--- pmm/pmm2-ovf-staging-start.groovy
+++ pmm/pmm2-ovf-staging-start.groovy
@@ -276,7 +276,7 @@ pipeline {
                                 sudo yum install -y yum-utils
                                 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
                                 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                                gpasswd -a $USER docker
+                                sudo gpasswd -a $USER docker
                                 sudo systemctl start docker
 
                                 export PATH=$PATH:/usr/sbin
