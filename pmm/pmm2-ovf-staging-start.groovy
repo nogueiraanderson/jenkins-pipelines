e7f5b4de167bd2d80bff4be4a87f04fcbb2521dc
Peter Sirotnak <sirotnak.p@gmail.com>
2024-09-06 13:50:13 +0200

    PMM-7 Assign user to docker

--- pmm/pmm2-ovf-staging-start.groovy
+++ pmm/pmm2-ovf-staging-start.groovy
@@ -276,7 +276,8 @@ pipeline {
                                 sudo yum install -y yum-utils
                                 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
                                 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                                
+                                gpasswd -a $USER docker
+                                sudo systemctl start docker
 
                                 export PATH=$PATH:/usr/sbin
                                 export PMM_CLIENT_VERSION=${CLIENT_VERSION}
