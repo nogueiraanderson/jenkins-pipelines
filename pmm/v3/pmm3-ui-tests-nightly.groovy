5e95dae6f80feb5629ddeda8512dd6dc0ddf78bb
Peter Sirotnak <sirotnak.p@gmail.com>
2025-02-01 21:21:55 +0100

    PMM-7: Test

--- pmm/v3/pmm3-ui-tests-nightly.groovy
+++ pmm/v3/pmm3-ui-tests-nightly.groovy
@@ -112,6 +112,7 @@ void checkClientNodesAgentStatus(String VM_CLIENT_IP, PMM_QA_GIT_BRANCH) {
     withCredentials([sshUserPrivateKey(credentialsId: 'aws-jenkins', keyFileVariable: 'KEY_PATH', passphraseVariable: '', usernameVariable: 'USER')]) {
         sh """
             ssh -i "${KEY_PATH}" -o ConnectTimeout=1 -o StrictHostKeyChecking=no ${USER}@${VM_CLIENT_IP} '
+                echo $CI
                 set -o errexit
                 set -o xtrace
                 echo "Checking Agent Status on Client Nodes";
