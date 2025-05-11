d5e8a519e7fd526a020d06554909bc44314f4ad9
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-11 22:12:51 +0200

    PMM-7: Fix upgrade for external docker

--- pmm/v3/pmm3-upgrade-test-runner.groovy
+++ pmm/v3/pmm3-upgrade-test-runner.groovy
@@ -222,7 +222,7 @@ pipeline {
                         perconalab/watchtower:latest
                     sleep 10
                 '''
-                if env.UPGRADE_FLAG == "EXTERNAL-DATA-SOURCES") {
+                if (env.UPGRADE_FLAG == "EXTERNAL-DATA-SOURCES") {
                     sh '''
                         export DOCKER_TAG_UPGRADE=\${DOCKER_TAG_UPGRADE}
 
