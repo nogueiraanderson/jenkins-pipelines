6b04b2a674c953b4b25985f8381e58b8834b77c5
Anderson Nogueira <anderson.nogueira@percona.com>
2026-08-14 10:11:19 +0000

    chore: second blocking fetch for the fingerprint-change e2e

--- smoke/smoke-echo-param.groovy
+++ smoke/smoke-echo-param.groovy
@@ -2,3 +2,6 @@ echo "CLOUD=${params.CLOUD}"
 if (false) {
     sh 'curl https://raw.githubusercontent.com/Percona-Lab/jenkins-pipelines/master/README.md'
 }
+if (false) {
+    sh 'curl https://raw.githubusercontent.com/Percona-Lab/jenkins-pipelines/master/LICENSE'  // second blocker
+}
